171f4ee18f12fd17c4e532b011653b1604a3b523
Mohammad Hadiyan <hadiyanmohamad131@gmail.com>
2021-10-07 17:47:52 +0330

    eslint configuration

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+    "settings":{
+        "react":{
+            "version":"detect"
+        }
+    },
+    "env":{
+        "browser":true,
+        "es2021":true,
+        "node":true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+        "plugin:jest/recommended",
+        "plugin:jest/style",
+        "plugin:testing-library/react",
+        "next",
+        "next/core-web-vitals"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "project":"./tsconfig.json",
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "plugins":["react"],
+    "rules": {
+        "@typescript-eslint/explicit-module-boundary-types":"off",
+        "react/react-in-jsx-scope":"off",
+        "import/no-anonymous-default-export":"off"
+    }
+};
