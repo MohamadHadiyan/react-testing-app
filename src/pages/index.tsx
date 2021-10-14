a590e1e6410f67185900df07b911231b4ae93816
Mohammad Hadiyan <hadiyanmohamad131@gmail.com>
2021-10-14 15:50:51 +0330

    install @ matrial-ui/core and @mui/x-data-grid packages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,16 @@
 import Counter from "../components/counter/Counter";
+import GiveMoney from "../components/moking_examples/table_data/TableData";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  const onMoney = (num: number) => {
+    console.log(num);
+  };
+
   return (
     <div className={styles.container}>
-      <Counter defaultCount={10} description="My Counter" />
+      {/* <Counter defaultCount={10} description="My Counter" /> */}
+      <GiveMoney onMoney={onMoney} />
     </div>
   );
 }
